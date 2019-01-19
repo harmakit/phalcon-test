6f31f1a76245edbd696a1ed848b2b562ab31e732
harmakit <harmakit@gmail.com>
2019-01-19 19:42:59 +0700

    bugfixes

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -7,16 +7,31 @@ class IndexController extends ControllerBase
     {
         $start = microtime(true);
 
-        $children = $this->db->query('
-            SELECT id, name, parent_id 
-            FROM (SELECT * FROM region
-                  ORDER BY parent_id, id) regions_sorted,
-                 (SELECT @pv := \'' . $id . '\') initialization,
-                 (SELECT @orig := \'' . $id . '\') initialization2
-            WHERE (FIND_IN_SET(parent_id, @pv)
-            AND LENGTH(@pv := concat(@pv, \',\', id)))
-            OR id = @orig
-        ')->fetchAll();
+        if ($id > 0) {
+            $children = $this->db->query('
+                SELECT id, name, parent_id 
+                FROM (SELECT * FROM region
+                      ORDER BY parent_id, id) regions_sorted,
+                     (SELECT @pv := \'' . $id . '\') initialization,
+                     (SELECT @orig := \'' . $id . '\') initialization2
+                WHERE (FIND_IN_SET(parent_id, @pv)
+                AND LENGTH(@pv := concat(@pv, \',\', id)))
+                OR id = @orig;
+            ');
+            $children->setFetchMode(\Phalcon\Db::FETCH_ASSOC);
+            $children = $children->fetchAll();
+        }
+
+        if (empty($children)) {
+            $data = [
+                'people' => [],
+                'time' => microtime(true) - $start,
+                'memory' => (memory_get_usage(true) / 1024) / 1024 . ' MB'
+            ];
+
+            $this->response->setJsonContent($data);
+            return $this->response;
+        }
 
         $childrenIdentifiers = [];
         foreach ($children as $key => $child) {
@@ -28,13 +43,35 @@ class IndexController extends ControllerBase
 
         $randomPeople = [];
         for ($i = 0; $i < 3; $i++) {
-            $randomPeople[] = $this->db->query('
-            SELECT p.* FROM people p
-            JOIN ( SELECT RAND() * (SELECT MAX(id) FROM people) AS max_id ) AS m
-            WHERE p.id >= m.max_id
-            AND p.region_id in ' . $childrenIdentifiers . '
-            ORDER BY p.id ASC
-            LIMIT 1;')->fetch();
+            $rand = (float)mt_rand() / (float)mt_getrandmax();
+            $row = false;
+            while (!$row) {
+                $row = $this->db->query('
+                    SELECT p.* FROM people p
+                    JOIN ( SELECT ' . $rand . ' * (SELECT MAX(id) FROM people) AS max_id ) AS m
+                    WHERE p.id >= m.max_id
+                    AND p.region_id in ' . $childrenIdentifiers . '
+                    ORDER BY p.id ASC
+                    LIMIT 1;
+                ');
+                $row->setFetchMode(\Phalcon\Db::FETCH_ASSOC);
+                $row = $row->fetch();
+
+                if (in_array($row, $randomPeople)) {
+                    $row = false;
+                }
+
+                if ($rand <= 0.01) {
+                    $rand = 0;
+                } else {
+                    $rand /= 1.2;
+                }
+
+                if ($rand === 0) {
+                    break;
+                }
+            }
+            $randomPeople[] = $row;
         }
 
         $findPath = function ($currentParentId, $path = []) use (&$findPath, $children) {
@@ -53,7 +90,7 @@ class IndexController extends ControllerBase
             if (null !== $parent) {
                 $path = $findPath($parent['parent_id'], $path);
             }
-             return $path;
+            return $path;
         };
 
         $people = [];
@@ -67,7 +104,7 @@ class IndexController extends ControllerBase
             }
             $path = $findPath($currentRegion['parent_id']);
             $pathString = '';
-            for ($i = count($path) - 1; $i >=0; $i--) {
+            for ($i = count($path) - 1; $i >= 0; $i--) {
                 $pathString .= $path[$i]['name'] . '[' . $path[$i]['id'] . '] -> ';
             }
             $pathString .= $currentRegion['name'] . '[' . $currentRegion['id'] . ']';
@@ -82,7 +119,8 @@ class IndexController extends ControllerBase
 
         $data = [
             'people' => $people,
-            'time' => microtime(true) - $start
+            'time' => microtime(true) - $start,
+            'memory' => (memory_get_usage(true) / 1024) / 1024 . ' MB'
         ];
 
         $this->response->setJsonContent($data);
